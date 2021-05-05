bbd6bcbbe0f32c440bb47d26ce726bc03ecd0078
Ray Anthony <diverraya@gmail.com>
2021-05-05 11:54:16 -0700

    Wip

--- Nerdino.CMS.Portal/ClientApp/src/components/Layout.js
+++ Nerdino.CMS.Portal/ClientApp/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
+import {Header} from "./Header";
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -9,6 +10,7 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
+          <Header />
         <Container>
           {this.props.children}
         </Container>
